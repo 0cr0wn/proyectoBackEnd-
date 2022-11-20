03db0a1f88b0f54cc957d02c271102b533934ba6
Ocrown <tuliocrown@gmail.com>
2022-11-20 19:44:35 -0300

    desafio act

---
+++ desafio8/src/utils/messages.js
@@ -0,0 +1,11 @@
+
+import moment from "moment";
+
+export const formatMessages = (data) => {
+    const { email, message } = data;
+    return {
+        email,
+        message,
+        time: moment().format(),
+    };
+};
