a73abe84dce35e82ced56134258e782c55018203
Ocrown <tuliocrown@gmail.com>
2022-12-22 20:04:15 -0300

    actualizando segunda entrega

--- proyectoF/src/services/server.js
+++ proyectoF/src/services/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import mainRouter from "../routes/main.js";
+import mainRouter from "../routes/index.js";
+import http from "http";
 
 const app = express();
 
@@ -8,11 +9,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", mainRouter);
 
-app.use((req, res, next) => {
-    return res.status(404).json({
-        error: -2,
-        descripcion: `ruta ${req.url} no implementada`,
-    });
+app.use((req, res) => {
+  return res.status(404).json({
+    error: -2,
+    descripcion: `ruta ${req.url} no implementada`,
+  });
 });
 
-export default app;
+app.use(function (error, res) {
+  return res.status("500").json({
+    mensaje: "hubo un problema inesperado!",
+    error: error.message,
+  });
+});
+
+const httpServer = http.Server(app);
+
+export default httpServer;
